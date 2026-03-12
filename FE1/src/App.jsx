c080f4662e197d486672016c9eda27603f92f38e
Only-San <duysannguyen113@gmail.com>
2026-03-12 20:48:05 +0700

    fix navigate

--- FE1/src/App.jsx
+++ FE1/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route,Navigate } from 'react-router-dom';
 import { userAPI } from './services/api';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
