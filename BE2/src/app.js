c314bc59796e0fe0b272208b6dfb22651e39b8b8
Only-San <duysannguyen113@gmail.com>
2026-03-14 17:19:52 +0700

    port

--- BE2/src/app.js
+++ BE2/src/app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-// Nạp file .env từ thư mục gốc
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
 const express = require('express');
@@ -9,21 +8,16 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(express.json());
 
-// Kết nối Database
 connectDB();
 
-// Routes
 app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {
-    res.json({ message: 'API is running 🚀' });
+  res.json({ message: 'API is running 🚀' });
 });
 
-
-app.listen(PORT, '0,0,0,0', () => {
-    console.log(`Server running on port ${PORT}`);
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
