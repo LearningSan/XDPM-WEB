dd2fa1ca684f45438001b8638c99121483f0f09b
PhongNguyen61004 <phongongp121@gmail.com>
2026-04-23 20:33:19 +0700

    complete

--- BE2/src/models/User.js
+++ BE2/src/models/User.js
@@ -1,37 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-<<<<<<< HEAD
-  
-  username: {
-    type: String,
-    required: [true, 'Name is required'],
-    trim: true,
-  },
-  role: {
-    type: String,
-    enum: ['user',  'admin'], 
-    default: 'user'
-  },
-  email: {
-  type: String,
-  required: [true, 'Email is required'],
-  unique: true,      
-  lowercase: true,   
-  trim: true
-  },
-password: {
-    type: String,
-    
-    default:''
-  },
-  avatar: {
-    type: String,
-    default: 'https://i.pravatar.cc/150' // Ảnh mặc định nếu user chưa tải lên
-  },
-}, {
-  timestamps: true,
-=======
+
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true }, // Chú ý: Đổi từ 'password' thành 'passwordHash'
@@ -46,7 +16,7 @@ password: {
   
   createdAt: { type: Date, default: Date.now }
 
->>>>>>> eaf6327 (update for new db)
+
 });
 
 module.exports = mongoose.model('User', userSchema);
